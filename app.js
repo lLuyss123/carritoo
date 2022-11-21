6259031625ea3506fddf11dbf7a2ba0a4ea7c1d7
Luis Mejia <lmejiaidep@gmail.com>
2022-11-21 17:58:29 -0500

    2

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const productos = document.getElementById('productos');
-document.addEventListener('DOMContentLoaded', e => {fetchData()});
 const fragment = document.createDocumentFragment();
+let carrito ={};
+const templateProductos = document.getElementById("template-productos").content;
+
+
+
+document.addEventListener('DOMContentLoaded', e => {fetchData()});
+productos.addEventListener('click',e =>{agregarCarrito(e)});
+
 const fetchData = async () =>{
     const res = await fetch('api.json');
     const data = await res.json();
@@ -8,14 +15,28 @@ const fetchData = async () =>{
 }
 
 const pintarCards = data =>{
-    const templateProductos = document.getElementById("template-productos").content;
     data.forEach(item => {
-        templateProductos.querySelector('h5').textContent=item.titulo;
-        templateProductos.querySelector('p').textContent=item.precio;
-        templateProductos.querySelector('img').setAttribute("src", item.imagen);
+        templateProductos.querySelector('h5').textContent=item.name;
+        templateProductos.querySelector('p').textContent="$"+item.precio;
+        templateProductos.querySelector('img').setAttribute("src", item.image);
         templateProductos.querySelector('button').dataset.id=item.id;
         const clone = templateProductos.cloneNode(true);
         fragment.appendChild(clone);
     });
     productos.appendChild(fragment);
+}
+
+const agregarCarrito = e =>{
+    llenarCarrito(e.target.parentElement);
+}
+
+const llenarCarrito = item =>{
+    const producto={
+        id: item.querySelector('button').dataset.id,
+        precio: item.querySelector('p').textContent,
+        titulo: item.querySelector('h5').textContent,
+        cantidad:1
+    }
+
+    carrito(producto.id)={...producto};
 }
